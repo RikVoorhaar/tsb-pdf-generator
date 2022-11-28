47968d7cc2c15c1080fc1cd81e9aa3f86d6c0c8b
Rik Voorhaar <wh.voorhaar@gmail.com>
2022-11-28 17:23:27 +0100

    restructure project

--- app.py
+++ app.py
@@ -1,95 +1,20 @@
-import os
-import datetime
-import json
+from pathlib import Path
 
-from flask import Flask, send_file, send_from_directory, render_template, request
-from werkzeug.utils import secure_filename
-
+from flask import Flask, send_file, request
+from flask_api import status
 from make_pdf import make_pdf
 
-try:
-    os.mkdir("tmp")
-except FileExistsError:
-    pass
 app = Flask(__name__)
-app.secret_key = "secret key"
-UPLOAD_FOLDER = "tmp"
-# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-
-SUBJECTS = (
-    "Earth & Space",
-    "Evolution & Behaviour",
-    "Health & Physiology",
-    "Maths, Physics & Chemistry",
-    "Microbiology",
-    "Neurobiology",
-    "Plant Biology",
-    "Psychology",
-)
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    vars = dict()
-    num_affiliations = 5
-    num_authors = 3
-    vars["affiliation_ids"] = [str(i) for i in range(1, num_affiliations + 1)]
-    vars["author_ids"] = [str(i) for i in range(1, num_authors + 1)]
-    vars["subjects"] = SUBJECTS
-    today = datetime.date.today()
-    vars["today"] = today.strftime("%B %d, %Y")
-    error_message = ""
-    if request.method == "POST":
-        # Create temp directory
-        try:
-            os.mkdir("tmp")
-        except FileExistsError:
-            pass
-
-        # Handle the cover image upload
-        file = request.files["coverImage"]
-        if file.filename != "":
-            filename = secure_filename(file.filename)
-        elif request.form["fileName"] != "":
-            filename = request.form["fileName"]
-        else:
-            filename = ""
-            error_message += "No cover image selected\n"
-        vars["fileName"] = filename
-
-        filetype = filename.split(".")[-1]
-        if filename != "" and filetype not in ["jpg", "png", "jpeg"]:
-            error_message = "Invalid file type"
-        elif file.filename != "":
-            file.save(os.path.join("tmp", filename))
-
-        # Check if subject has been selected
-        if request.form["subjectSelect"] == "Choose subject...":
-            error_message += "Please select a subject\n"
-
-        # Check length of abstract
-        abstract = request.form["abstractText"]
-        if len(abstract) > 400:
-            error_message += "Abstract is too long\n"
-
-        if error_message == "":
-            with open(os.path.join("tmp", "metadata.json"), "w") as f:
-                metadata = request.form.to_dict()
-                metadata["fileName"] = filename
-                json.dump(metadata, f, indent=2)
-
-        # Make the PDF, and download it
-        if error_message == "":
-            make_pdf()
-            print("PDF built")
-            print(os.listdir("tmp"))
-            try:
-                return send_file("tmp/main.pdf", as_attachment=True)
-            except FileNotFoundError:
-                print("PDF not found")
+@app.route("/", methods=["POST"])
+def post_form():
+    data = request.json
+    pdf_location = make_pdf(data)
+    return pdf_location, status.HTTP_201_CREATED
 
 
-    if error_message != "":
-        vars["error_message"] = error_message
-
-    return render_template("index.html", **vars)
+@app.route("/pdf/<filename>", methods=["GET"])
+def get_pdf(filename):
+    pdf_folder = Path("pdf")
+    return send_file(pdf_folder / filename, as_attachment=True)
--- make_pdf.py
+++ make_pdf.py
@@ -1,16 +1,29 @@
-# %%
+import logging
 import os
-import datetime
-import shutil
+import re
 import subprocess
-import json
-
-
-TMP_LOCATION = "tmp"
+from datetime import datetime
+from pathlib import Path
 
 import jinja2
+import pandoc
+import requests
 
-latex_jinja_env = jinja2.Environment(
+# Map category ID to correct names
+CAT_ID_TO_NAME = {
+    1: "Health \& Physiology",
+    2: "Neurobiology",
+    3: "Earth \& Space",
+    4: "Evolution \& Behaviour",
+    5: "Plant Biology",
+    6: "Microbiology",
+    7: "Maths, Physics \& Chemistry",
+    13: "Psychology",
+}
+
+TEMPLATE_PATH = Path("templates") / "template.tex"
+
+LATEX_JINJA_ENV = jinja2.Environment(
     block_start_string="\BLOCK{",
     block_end_string="}",
     variable_start_string="\VAR{",
@@ -21,65 +34,37 @@ latex_jinja_env = jinja2.Environment(
     line_comment_prefix="%#",
     trim_blocks=True,
     autoescape=False,
-    loader=jinja2.FileSystemLoader(os.path.abspath(".")),
+    loader=jinja2.FileSystemLoader(Path(".").resolve()),
 )
-TEMPLATE_LOCATION = "templates/template.tex"
-
-TMP_LOCATION = "tmp"
 
 
-def make_tex(preamb_file, metadata_file):
-    """Turn the data into a pdf"""
-    try:
-        os.mkdir("tmp")
-    except FileExistsError:
-        pass
+def html_to_latex(html_text):
+    """Convert HTML to LaTeX using pandoc"""
+    if html_text is None:
+        return ""
+    Path("tmp").mkdir(exist_ok=True)
 
-    main_file = "tmp/main.tex"
+    doc = pandoc.read(html_text, format="html")
+    result = pandoc.write(doc, "tmp/html_text.tex", format="latex")
 
-    with open(metadata_file, "r") as f:
-        metadata = json.load(f) 
+    Path("tmp/html_text.tex").unlink()
 
-    template = latex_jinja_env.get_template(TEMPLATE_LOCATION)
-
-    date = metadata["date"]
-    if date == "":
-        today = datetime.date.today()
-        date = today.strftime("%B %d, %Y")
-    metadata["date"] = date
-
-    authors = _process_authors(metadata)
-    metadata["authorText"] = _make_authors_text(authors)
-
-    affiliations = _process_affiliations(metadata)
-    metadata["affiliationText"] = _make_affiliations_text(affiliations)
-
-    metadata["subjectSelect"] = escape_ampersand(metadata["subjectSelect"])
-
-    with open(main_file, "w") as f:
-        f.write(template.render(**metadata))
+    return result.rstrip()
 
 
-def _process_authors(metadata):
-    """Process the authors"""
-    ids_ = []
-    authors = []
-    for key in metadata.keys():
-        if key.startswith("author"):
-            ids_.append("".join([s for s in key if s.isdigit()]))
-    for id in ids_:
-        author_name = (
-            metadata["firstName" + id] + " " + metadata["lastName" + id]
-        )
-        if metadata["affiliation" + id].strip() != "":
-            authors.append(
-                {
-                    "name": author_name,
-                    "affiliation": metadata["affiliation" + id],
-                    "title": metadata["authorTitle" + id],
-                }
-            )
-    return authors
+def escape_chars(s):
+    """Escape characters that are special in LaTeX"""
+    s = s.replace("&", r"\&")
+    s = s.replace("_", r"\_")
+    s = s.replace("%", r"\%")
+    s = s.replace("$", r"\$")
+    s = s.replace("#", r"\#")
+    s = s.replace("{", r"\{")
+    s = s.replace("}", r"\}")
+    s = s.replace("^", r"\textasciicircum{}")
+    s = s.replace("[", "\[")
+    s = s.replace("]", "\]")
+    return s
 
 
 def _make_authors_text(authors):
@@ -88,14 +73,14 @@ def _make_authors_text(authors):
     out += "\n" + r"\vspace{-1ex}{by " + "\small"
     for i, author in enumerate(authors):
         out += r"\textbf{\textcolor{AuthorColor}{\small "
-        out += author["name"]
+        out += escape_chars(author["name"])
         out += "}}"
 
         out += r"$^{"
-        out += author["affiliation"]
+        out += escape_chars(str(author["affiliation"]))
         out += r"}$ "
 
-        out += author["title"]
+        out += escape_chars(author["title"])
 
         if i != len(authors) - 1:
             out += r" $\mid$ "
@@ -104,59 +89,171 @@ def _make_authors_text(authors):
     return out
 
 
-def _process_affiliations(metadata):
-    """Process the affiliations"""
+def _process_authors(authors):
+    """Process the authors and affiliations"""
     affiliations = {}
-    for key, value in metadata.items():
-        if key.startswith("inputAffiliation"):
-            id = "".join([s for s in key if s.isdigit()])
-            if value.strip() != "":
-                affiliations[id] = value
-    return affiliations
+    author_dicts = []
+    affil_counter = 1
+    for author in authors:
+        if author["research_institute"] not in affiliations:
+            affiliations[author["research_institute"]] = affil_counter
+            affil_counter += 1
+        author_dicts.append(
+            {
+                "name": (author["first_name"] + " " + author["last_name"]),
+                "affiliation": affiliations[author["research_institute"]],
+                "title": (author["position"]),
+            }
+        )
+
+    return author_dicts, affiliations
 
 
 def _make_affiliations_text(affiliations):
     """Insert the affiliations into the main file"""
     out = ""
-    out += "\n" + r"{\footnotesize "
-    for key, affiliation in affiliations.items():
-        out += r"${}^" + key + r"$: " + affiliation + r"\\ "
+    out += r"{"
+    for affiliation, affil_id in affiliations.items():
+        out += (
+            r"${}^"
+            + str(affil_id)
+            + r"$: "
+            + escape_chars(affiliation)
+            + r" \\ "
+        )
     out += r"}"
     return out
 
 
-def escape_ampersand(string):
-    return string.replace("&", "\\&")
+def force_straight_quotes(text):
+    """
+    Enforce straight quotes to avoid issues with unmatched quotes
+    """
+    text = text.replace(r'" ', r"\textquotedbl{} ")
+    text = text.replace(r'"', r"\textquotedbl ")
+    return text
 
 
-def make_pdf():
-    make_tex(
-        "preamb.tex",
-        "tmp/metadata.json",
+def remove_hypertarget(text):
+    """
+    Fixes bug with brk611 which has an embedded video.
+
+    This removes the video entirely.
+    """
+    text = re.sub(r"\\hypertarget{.*?}", "", text)
+    text = re.sub(r"\\includegraphics{.*?}", "", text)
+    return text
+
+
+def _get_img(data):
+    """Download and save the image from the article"""
+    tmp = Path("tmp")
+    image_name = Path(data["image_path"]).name
+    image_path = data["image_path"]
+    if not image_path.startswith("http"):
+        img_url = "https://thesciencebreaker.org/" + data["image_path"]
+    else:
+        img_url = image_path
+    img_filename = tmp / image_name
+    if img_filename.exists():
+        logging.info("Image already downloaded, skipping")
+        return image_name, 200
+
+    logging.info("Downloading image %s", img_url)
+    resp = requests.get(img_url)
+    logging.info("Status code: %s", resp.status_code)
+    if resp.status_code == 200:
+        with open(img_filename, "wb") as f:
+            f.write(resp.content)
+        subprocess.run(
+            ["convert", img_filename, "-colorspace", "RGB", img_filename],
+        )
+
+    return image_name, resp.status_code
+
+
+def process_data(data):
+    """
+    Process the data from the API to make it ready for the LaTeX template.
+    """
+    template_data = {}
+
+    main_text = html_to_latex(data["content"])
+    main_text = force_straight_quotes(main_text)
+    main_text = remove_hypertarget(main_text)
+    template_data["mainText"] = main_text
+
+    title = escape_chars(data["title"].replace("\n", " "))
+    template_data["subjectTitle"] = title
+    logging.info(f"Title: {title}")
+    logging.info(f"DOI: {data['doi']}")
+    template_data["abstractText"] = html_to_latex(data["description"])
+    if len(template_data["abstractText"]) > 0:
+        template_data["useAbstract"] = r"\abstracttrue"
+    else:
+        template_data["useAbstract"] = r"\abstractfalse"
+    published_time = datetime.strptime(
+        data["published_at"], "%Y-%m-%d %H:%M:%S"
     )
+    template_data["date"] = published_time.strftime("%B %d, %Y")
+    template_data["subjectSelect"] = CAT_ID_TO_NAME[data["category_id"]]
+
+    authors, affiliations = _process_authors(data["authors"])
+    template_data["authorText"] = _make_authors_text(authors)
+    template_data["affiliationText"] = _make_affiliations_text(affiliations)
+    template_data["editorName"] = f"scientific editor \# {data['editor_id']}"
+
+    template_data["fileName"], status_code = _get_img(data)
+    if status_code != 200:
+        template_data["useImage"] = "\imagefalse"
+    else:
+        template_data["useImage"] = "\imagetrue"
+    template_data["imageCredits"] = html_to_latex(data["image_credits"])
+
+    template_data["citation"] = html_to_latex(data["original_article"])
+    return template_data
+
+
+def make_latex(template_data):
+    """
+    Make the latex file from the template and the data
+    """
+    tmp = Path("tmp")
+    main_file = tmp / "main.tex"
+    template = LATEX_JINJA_ENV.get_template(TEMPLATE_PATH.as_posix())
+
+    with open(main_file, "w") as f:
+        f.write(template.render(**template_data))
+
+
+def make_pdf(data):
+    """Make the pdf from the json file"""
+    tmp = Path("tmp")
+    tmp.mkdir(exist_ok=True)
+    pdf_folder = Path("pdf")
+    pdf_folder.mkdir(exist_ok=True)
+
+    logging.info("Processing data")
+    template_data = process_data(data)
+    logging.info("Making LaTeX")
+    make_latex(template_data)
 
     # Now we can compile the tex-file
-    print("Compiling PDF...")
+    logging.info("Compiling PDF")
     tex_log = subprocess.run(
-        ["latexmk", "-pdf", "-interaction=nonstopmode", "main.tex"],
+        ["xelatex", "-interaction=nonstopmode","-papersize=a4", "main.tex"],
         capture_output=True,
-        cwd=TMP_LOCATION,
+        cwd=tmp,
     )
     num_errors = 0
     for line in tex_log.stdout.decode("utf-8").split("\n"):
         if line.startswith("!"):
-            print("\n")
-            print(line)
-            print("\n")
+            logging.error("LaTeX error: " + line)
             num_errors += 1
-    if num_errors > 0:
-        print("Some errors were encountered with latex. Entire log:")
-        print(tex_log.stdout.decode("utf-8"))
-    else:
-        print("PDF compiled without errors!")
+    logging.info("Done compiling PDF, %d errors", num_errors)
 
+    pdf_name = f"{data['slug']}.pdf"
+    pdf_filename = pdf_folder / pdf_name
+    os.rename(tmp / "main.pdf", pdf_filename)
 
-if __name__ == "__main__":
-    make_pdf()
-
-# %%
+    return pdf_filename.as_posix()
