691f4aaca213cfc04a53a67a9ddb89eb631a81e8
Rik Voorhaar <wh.voorhaar@gmail.com>
2023-01-07 15:36:26 +0100

    update test_post_get.py

--- app/test/test_post_get.py
+++ app/test/test_post_get.py
@@ -4,15 +4,20 @@ import requests
 from pathlib import Path
 
 files = list(Path("json").glob("*.json"))
-json_file = Path("json") / "break_720.json"
+
+json_file = Path("json") / "break_274.json"
 data = json.loads(json_file.read_text())
 
 # %%
-url = "http://localhost:5000/"
-resp_post = requests.post(url, json=data)
-# %%
-pdf_filename = resp_post.text
+url = "http://tsb.rikvoorhaar.com/"
+# url = "http://127.0.0.1:5000/"
+response = requests.post(url, json=data)
+
+pdf_filename = response.text
+reponse = requests.get(url + pdf_filename)
 print(pdf_filename)
-resp_get = requests.get(url + pdf_filename)
 # %%
-len(resp_get.content)
+with open(pdf_filename, "wb") as f:
+    f.write(reponse.content)
+
+# %%
